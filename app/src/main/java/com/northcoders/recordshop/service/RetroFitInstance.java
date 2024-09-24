0121ee3ae29e7e9dafb20c705fb4f5bad73d5b6e
Trntybth <trinitybooth89@gmail.com>
2024-09-24 09:42:16 +0100

    feat: created service layer package and RetroFitInstance class

---
+++ app/src/main/java/com/northcoders/recordshop/service/RetroFitInstance.java
@@ -0,0 +1,11 @@
+package com.northcoders.recordshop.service;
+import retrofit2.Retrofit;
+
+
+public class RetroFitInstance {
+    private static Retrofit retrofit = null;
+    private static final String baseURL = "http://10.0.2.2:8080/api/v1/"; // double check this later ?
+
+
+    }
+}
