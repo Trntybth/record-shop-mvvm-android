56aada5440e87204f7f54cd3e205013a32dbe533
Trntybth <trinitybooth89@gmail.com>
2024-09-24 10:13:13 +0100

    feat: implemented overridden methods in MainActivity, added fields and created displayInRecyclerView method. also gave activity_main an id of 'main' to prevent error

--- app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivity.java
@@ -1,22 +1,70 @@
 package com.northcoders.recordshop.ui.mainactivity;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
 
 import android.os.Bundle;
 
 import com.northcoders.recordshop.R;
+import com.northcoders.recordshop.databinding.ActivityMainBinding;
 import com.northcoders.recordshop.model.Album;
-import com.northcoders.recordshop.model.AlbumRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Album> albumList;
+    private AlbumAdapter albumAdapter;
+    private MainActivityViewModel mainActivityViewModel;
+    private ActivityMainBinding activityMainBinding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        getAllAlbums();
     }
+
+    private void getAllAlbums() {
+        mainActivityViewModel.getAllAlbums().observe(this, new Observer<List<Album>>() {
+            @Override
+            public void onChanged(List<Album> albumsFromLiveData) {
+                albumList = (ArrayList<Album>) albumsFromLiveData;
+                displayInRecyclerView();
+            }
+        });
+    }
+
+    private void displayInRecyclerView(){
+        recyclerView = activityMainBinding.recyclerView;
+        albumAdapter = new AlbumAdapter(albumList, this);
+        recyclerView.setAdapter(albumAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        albumAdapter.notifyDataSetChanged();
+    }
+
+
 }
