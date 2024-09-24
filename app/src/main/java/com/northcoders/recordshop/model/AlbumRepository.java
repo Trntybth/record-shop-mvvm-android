82b4b02dfa3d252590c472c81875b9b408d70b4d
Trntybth <trinitybooth89@gmail.com>
2024-09-24 09:50:27 +0100

    feat: added getMutableLiveData() method

--- app/src/main/java/com/northcoders/recordshop/model/AlbumRepository.java
+++ app/src/main/java/com/northcoders/recordshop/model/AlbumRepository.java
@@ -2,6 +2,10 @@ package com.northcoders.recordshop.model;
 import android.app.Application;
 import androidx.databinding.BaseObservable;
 import androidx.lifecycle.MutableLiveData;
+
+import com.northcoders.recordshop.service.AlbumApiService;
+import com.northcoders.recordshop.service.RetroFitInstance;
+
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -11,6 +15,25 @@ public class AlbumRepository {
     private Application application;
     public AlbumRepository(Application application) {
         this.application = application;
+    }
+    public static MutableLiveData<List<Album>> getMutableLiveData() {
+
+        AlbumApiService albumApiService = RetroFitInstance.getService();
+        Call<List<Album>> call = albumApiService.getAllAlbums();
+        call.enqueue(new Callback<List<Album>>() {
+
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                List<Album> albumList = response.body();
+                mutableLiveData.postValue(albumList);
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable throwable) {
+                System.out.println("Failure: " + throwable.getMessage());
+            }
+        });
+        return mutableLiveData;
     }
 
 }
