72f951677860bd4cdeacd4f94059be86563450f2
Trntybth <trinitybooth89@gmail.com>
2024-09-24 09:48:23 +0100

    feat: added getService() method to RetroFitInstance

--- app/src/main/java/com/northcoders/recordshop/service/RetroFitInstance.java
+++ app/src/main/java/com/northcoders/recordshop/service/RetroFitInstance.java
@@ -1,11 +1,30 @@
 package com.northcoders.recordshop.service;
-import retrofit2.Retrofit;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroFitInstance {
     private static Retrofit retrofit = null;
-    private static final String baseURL = "http://10.0.2.2:8080/api/v1/"; // double check this later ?
+    private static final String baseURL = "http://10.0.2.2:8080/api/v1/";
 
+    public static AlbumApiService getService() {
+        if (retrofit == null) {
+            // this allows us to log the JSON body of the HTTP request
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit.create(AlbumApiService.class);
 
     }
 }
